2bf820824601618377cda02a7d1b69199535f34f
ParkDoYoung <ehdud5578@gamil.com>
2023-05-20 11:27:44 +0900

    init : init project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <HelloExample />
+  </div>
+</template>
+<script>
+import HelloExample from "@/components/HelloExample.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    HelloExample,
+  },
+};
+</script>
